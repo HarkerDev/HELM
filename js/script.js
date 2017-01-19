3254df1278eb165ae20e42b2b3e036d5a789ccdc
Andrew Tierno <andrew.tierno@gmail.com>
2017-01-19 12:53:47 -0800

    Added moving with arrow keys

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
+var NUM_PAGES = 13;
+
+var KEY_LEFT = 37;
+var KEY_RIGHT = 39;
+
+var PAGE_MATCHER = /p\d{1,2}/
+var HOME_
+
+function getPageNumber(href) {
+    console.log(href);
+    return parseInt(PAGE_MATCHER.exec(href)[0].substring(1));
+}
+
+document.addEventListener("keydown", function(event) {
+    switch(event.keyCode) {
+        case KEY_LEFT: 
+            if (pageNumberCurr != 1) {
+                pageNumberNew = pageNumberCurr - 1;
+                pageNumberNewStr = ((pageNumberNew < 10) ? "0" : "") + pageNumberNew;
+                window.location.href = "https://harkerdev.github.io/helm/pages/p" + pageNumberNewStr + ".html";
+            }
+        break;
+        case KEY_RIGHT:
+            pageNumberCurr = getPageNumber(window.location.href);
+            if (pageNumberCurr != NUM_PAGES) {
+                pageNumberNew = pageNumberCurr + 1;
+                pageNumberNewStr = ((pageNumberNew < 10) ? "0" : "") + pageNumberNew;
+                window.location.href = "https://harkerdev.github.io/helm/pages/p" + pageNumberNewStr + ".html";
+            }
+        break;
+    }
+});
+
 /*
 * 
 * Credits to http://css-tricks.com/long-dropdowns-solution/
@@ -6,7 +39,7 @@
 var maxHeight = 400;
 $(function(){
     $(".dropdown > li").hover(function() {
-    
+        console.log("dropdown");
          var $container = $(this),
              $list = $container.find("ul"),
              $anchor = $container.find("a"),
